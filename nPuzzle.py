d85fe423d2831c77d413942bdd6dfcf138fe9d4b
Haroon Ahamed <56659007+Terraria200@users.noreply.github.com>
2026-02-13 16:13:40 -0800

    Add TreeNode class and utility functions for puzzle

--- nPuzzle.py
+++ nPuzzle.py
@@ -27,3 +27,38 @@ oh_boy = [[8, 7, 1],
 eight_goal_state = [[1, 2, 3],
                     [4, 5, 6],
                     [7, 8, 0]]
+
+
+
+class TreeNode:
+    def __init__(self, parent, board, g, h):
+        self.parent = parent
+        self.board = board
+        self.g = g
+        self.h = h
+        self.f = g + h
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+    def board_to_tuple(self):
+        return tuple(tuple(row) for row in self.board)
+
+    def solved(self):
+        return self.board == eight_goal_state
+
+
+# Utility functions_
+
+def print_puzzle(puzzle):
+    for row in puzzle:
+        print(row)
+    print()
+
+
+def find_blank(board):
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                return i, j
+
